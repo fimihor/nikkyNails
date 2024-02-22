714fee2c7278c521868f20dfbc5587f52f310efd
Fimihor Michael <fimihor61@gmail.com>
2024-02-22 01:24:14 -0800

    nikkys project

---
+++ app.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.style.display === 'none' ? navLinks.style.display = 'flex' : navLinks.style.display = 'none';
+  }
+  
